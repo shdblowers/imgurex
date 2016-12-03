911f3e0ebad713cf7b6be5f309d541095a8bb2b9
Steven Blowers <shdblowers@gmail.com>
2016-12-03 20:53:34 +0000

    adding test for getting file info

---
+++ test/imgurex/image_test.exs
@@ -0,0 +1,34 @@
+defmodule Imgurex.ImageTest do
+  use ExUnit.Case, async: false
+
+  alias Imgurex.Image
+
+  setup_all do
+    :meck.new(HTTPoison)
+    on_exit fn -> :meck.unload end
+    :ok
+  end
+
+  test "getting info for a ticket" do
+    expected = %Image{id: "Pc123G7",
+		      title: "Example Image",
+		      description: "picture of a test",
+		      datetime: 1480768844,
+		      type: "image/jpeg",
+		      animated: false,
+		      width: 1080,
+		      height: 1920,
+		      nsfw: nil,
+		      link: "http://i.imgur.com/Pc123G7.jpg"}
+
+    stub = fn("https://api.imgur.com/3/image/Pc123G7",
+              ["Authorization": "Client-ID 12149508e8b758f"]) ->
+	%HTTPoison.Response{body: Poison.encode!(%{data: expected})}
+    end
+    :meck.expect(HTTPoison, :get!, stub)
+
+    actual = Image.info!("Pc123G7", "12149508e8b758f")
+
+    assert expected == actual
+  end
+end
