bcabfd41a0fca0dcade4f10d9a298593b5eda419
Steven Blowers <shdblowers@gmail.com>
2016-12-03 20:30:45 +0000

    creating image struct

--- lib/imgurex/image.ex
+++ lib/imgurex/image.ex
@@ -1,5 +1,16 @@
 defmodule Imgurex.Image do
 
+  defstruct [:id,
+	     :title,
+	     :description,
+	     :datetime,
+	     :type,
+	     :animated,
+	     :width,
+	     :height,
+	     :nsfw,
+	     :link]
+
   @base_url Application.fetch_env!(:imgurex, :base_url)
 
   def info!(id, client_id) do
@@ -8,5 +19,19 @@ defmodule Imgurex.Image do
     |> Map.get(:body)
     |> Poison.decode!
     |> Map.get("data")
+    |> build_image_struct
+  end
+
+  defp build_image_struct(data) do
+    %__MODULE__{id: data["id"],
+                title: data["title"],
+                description: data["description"],
+		datetime: data["datetime"],
+		type: data["type"],
+		animated: data["animated"],
+		width: data["width"],
+		height: data["height"],
+		nsfw: data["nsfw"],
+		link: data["link"]}
   end
 end
