dcbe9abc739066fa77debbc4a3e2df0dfa0f274b
Steven Blowers <sblowers@findmypast.com>
2017-01-12 16:31:15 +0000

    upgrading elixir version to 1.4

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Imgurex.Mixfile do
   def project do
     [app: :imgurex,
      version: "0.1.0",
-     elixir: "~> 1.3",
+     elixir: "~> 1.4",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      deps: deps,
