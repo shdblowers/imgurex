e59bec00070dc584a3624873ab60636daaf14d4a
Steven Blowers <sblowers@gmail.com>
2017-01-29 20:28:46 +0000

    squash credo warnings around missing docs from imgur module

--- lib/imgurex/imgur.ex
+++ lib/imgurex/imgur.ex
@@ -1,8 +1,13 @@
 defmodule Imgurex.Imgur do
+@moduledoc"""
+Overrides for default HTTPoison methods to interact with Imgur API.
+"""
+
   use HTTPoison.Base
 
   @base_url Application.fetch_env!(:imgurex, :base_url)
 
+  @spec process_url(binary) :: binary
   def process_url(url) do
     @base_url <> url
   end
